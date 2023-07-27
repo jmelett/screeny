6eddcf7b75e7cd0766a26566d0f2986c068af4c0
Joseph Melettukunnel <jmelett@gmail.com>
2023-07-27 13:07:37 +0200

    Simplify by rendering image via template

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomeView, upload_screenshot, get_screenshot
+from .views import HomeView,  ScreenshotDetailView, upload_screenshot
 
 urlpatterns = [
     path('', HomeView.as_view(), name='screeny_project_root'), 
     path("send/", upload_screenshot, name="upload_screenshot"),
-    path("share/<uuid:uuid>/", get_screenshot, name="get_screenshot"),
+    path("share/<uuid:uuid>/",  ScreenshotDetailView.as_view(), name="get_screenshot"),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -5,20 +5,12 @@ from wsgiref.util import FileWrapper
 from django.conf import settings
 from django.db.models import F
 from django.http import FileResponse, Http404, HttpResponse, StreamingHttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 from rest_framework.decorators import api_view
 from .models import Screenshot
-
-
-class HomeView(TemplateView):
-    template_name = "base.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
 
 
 class FileResponseWithClose(FileResponse):
@@ -30,6 +22,32 @@ class FileResponseWithClose(FileResponse):
         if not self.closed and self.file_to_stream is not None:
             self.file_to_stream.close()
         super().close()
+
+
+class HomeView(TemplateView):
+    template_name = "base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class ScreenshotDetailView(DetailView):
+    model = Screenshot
+    template_name = "core/share_image.html"
+
+    def get_object(self):
+        # Overriding get_object to use uuid instead of default pk
+        uuid = self.kwargs.get("uuid")
+        return get_object_or_404(Screenshot, uuid=uuid)
+
+    def get(self, request, *args, **kwargs):
+        # Override the get method to increase the views count
+        self.object = self.get_object()
+        Screenshot.objects.filter(uuid=self.object.uuid).update(views=F("views") + 1)
+        self.object.refresh_from_db()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 @api_view(["POST"])
@@ -50,12 +68,12 @@ def upload_screenshot(request):
 
 
 @api_view(["GET"])
-def get_screenshot(request, uuid):
+def get_screenshot_reverse(request, uuid):
     screenshot = get_object_or_404(Screenshot, uuid=uuid)
     screenshot.views += 1
     screenshot.save()
 
-    if settings.DEFAULT_STORAGE_DSN.startswith('file://'):
+    if settings.DEFAULT_STORAGE_DSN.startswith("file://"):
         # If using local storage, open the file directly.
         file = open(screenshot.image.path, "rb")
         response = FileResponse(file)
